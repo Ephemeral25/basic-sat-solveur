81b9e4f195ed7bc8e3f9a7ae49f675d7b11b8b26
ephemeral25 <10870920+Ephemeral25@users.noreply.github.com>
2018-12-20 17:25:11 +0100

    + changes

--- cnfreader.c
+++ cnfreader.c
@@ -1,12 +1,9 @@
 /*
  * CNF READER source file
  */
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "cnfreader.h"
 
-const int VERBOSE = 0;
+const int VERBOSE = 1;
 
 FILE *open_file(const char *filename) {
     FILE *data;
@@ -14,16 +11,25 @@ FILE *open_file(const char *filename) {
     
     if(!data) {
         perror("[ERROR]: ");
-        return(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
     return data;
 }
 
-int lCount_file(char single) {
-
+int lCount_file(FILE *data) {
+    fseek(data, 0, SEEK_SET);
+    int n, c;
+    
+    while((c = fgetc(data) != EOF))
+        if(c == 10)
+            n++;
+    
+    if(VERBOSE)
+        printf("[INFO] : CNF File contains %d lines\n", n);
+    return n;   
 }
 
-int aCount_file(char single) {
+int aCount_file(FILE *data) {
     
 }
 
@@ -33,7 +39,7 @@ int aCountperLine_file(const char *filename) {
 
 void lTranslate_file(FILE *data) {
     //Place cursor at the begginning of the file
-    fseek(data, 0, SEEK_SET);
+    lCount_file(data);
     
     
 }
