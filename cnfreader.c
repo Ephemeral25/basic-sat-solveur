a0bbc1cbd357cc9c023d5d4f2dc771e8b9faf896
ephemeral25 <10870920+Ephemeral25@users.noreply.github.com>
2018-12-20 16:40:19 +0100

    + open_file function

--- cnfreader.c
+++ cnfreader.c
@@ -1,6 +1,39 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * CNF READER source file
  */
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "cnfreader.h"
+
+const int VERBOSE = 0;
+
+FILE *open_file(const char *filename) {
+    FILE *data;
+    data = fopen(filename, "r");
+    
+    if(!data) {
+        perror("[ERROR]: ");
+        return(EXIT_FAILURE);
+    }
+    return data;
+}
+
+int lCount_file(char single) {
+
+}
+
+int aCount_file(char single) {
+    
+}
+
+int aCountperLine_file(const char *filename) {
+    
+}
+
+void lTranslate_file(FILE *data) {
+    //Place cursor at the begginning of the file
+    fseek(data, 0, SEEK_SET);
+    
+    
+}
